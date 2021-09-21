27f9b83a71bfec0f13976dc543322670c62e9d4f
Etienne Boursault <43215408+EhBoss@users.noreply.github.com>
2021-09-21 23:39:44 +0200

    Gérez des données dynamiques

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { AppareilComponent } from './appareil/appareil.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MonPremierComponent
+    AppareilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

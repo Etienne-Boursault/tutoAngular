f3596c9c5a49c35bfbdc0fe68fd4819ad2acefc9
Etienne Boursault <43215408+EhBoss@users.noreply.github.com>
2021-10-09 21:24:43 +0200

    Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component} from '@angular/core';
+import { Observable } from 'rxjs'; // Besoin d'installer le package rxjs-compat via la commande "npm install --save rxjs-compat"
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +8,21 @@ import { Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
-  constructor() {  }
+
+  secondes: number = 0;
 
   ngOnInit() {
+    const counter = Observable.interval(1000);
+    counter.subscribe(
+      (value: number) => {
+        this.secondes = value;
+      },
+      (error: string) => {
+        console.log('Uh-oh, an error occured! : ' + error);
+      }, 
+      () => {
+        console.log('Observable complete!');
+      }
+    )
   }
 }
